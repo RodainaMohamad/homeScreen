c0837fd2c1bbd6b1470f4ae4660fb26f628886bc
sports_app <rodinamohamad2003@gmail.com>
2024-09-18 01:51:26 +0300

    homeScreen

---
+++ lib/ui/tabs/homePage/buildCategories.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:trial_hpg/data/model/response/CategoryDm.dart';
+import 'package:trial_hpg/data/model/response/ProductDM.dart';
+import 'package:trial_hpg/ui/tabs/homePage/widgets/categoryItem.dart';
+import 'package:trial_hpg/ui/tabs/homePage/widgets/productItem.dart';
+
+Widget buildCategories (List<categoryDM>list)=>
+    GridView.builder(
+      itemCount: list.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context,index){
+        return CategoryItem();
+      },
+    );
+
+
+Widget buildProducts (List<ProductDM>list)=>
+    ListView.builder(
+      itemCount: list.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context,index){
+        return Product();
+      },
+    );
